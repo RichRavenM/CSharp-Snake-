9b33fd08777b43653cbfd253f68278f282873c5f
RichRavenM <richard.s.morris@btinternet.com>
2023-10-30 15:31:47 +0000

    adds intro screen

--- Snake!/MainWindow.xaml.cs
+++ Snake!/MainWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace Snake_
         private readonly int rows = 15, cols = 15;
         private readonly Image[,] gridImages;
         private GameState gameState;
+        private bool gameRunning;
 
         public MainWindow()
         {
@@ -39,10 +40,27 @@ namespace Snake_
             gameState = new GameState(rows, cols);
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private async Task RunGame()
         {
             Draw();
+            await ShowCountdown();
+            Overlay.Visibility = Visibility.Hidden;
             await GameLoop();
+        }
+
+        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (Overlay.Visibility == Visibility.Visible)
+            {
+                e.Handled = true;
+            }
+            if (!gameRunning)
+            {
+                gameRunning = true;
+                await RunGame();
+                gameRunning = false;
+            }
+
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -107,6 +125,7 @@ namespace Snake_
             ScoreText.Text = $"Score: {gameState.Score}";
         }
 
+
         private void DrawGrid()
         {
             for (int i = 0; i < rows; i++)
@@ -116,6 +135,15 @@ namespace Snake_
                     GridValue gridVal = gameState.Grid[i, j];
                     gridImages[i, j].Source = gridValToImage[gridVal];
                 }
+            }
+        }
+
+        private async Task ShowCountdown()
+        {
+            for (int i = 3; i >= 1; i--)
+            {
+                OverlayText.Text = i.ToString();
+                await Task.Delay(500);
             }
         }
     }
